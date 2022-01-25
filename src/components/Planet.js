0b033372e1a39d920225d0debdff3919695ce289
Andrew Conte <andrew-conte434>
2022-01-25 12:38:45 -0500

    added Planet Component

---
+++ src/components/Planet.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function(props){
+    return (
+        <h1>{props.weatherData.main.temp}</h1>
+    )
+}  
--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,21 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import planet from "../images/hoth.jpeg"
+import Planet from "../components/Planet"
 import Axios from 'axios'
 import '../App.css'
 
 export default function ResultsPage(props) {
   
-  const [weatherData, setWeatherData] = useState([])
+  const [weatherData, setWeatherData] = useState(null)
+  const apiKey = 'c82709073957c8bbae4900187a3370f5'
 
-  useEffect(async() => {
-    const fetchData = () => {
-      console.log(props.city)
-    }
-    fetchData()
+  useEffect(async () => {
+    await fetchData()
+    console.log(weatherData)
   }, [])
 
+  const fetchData = async () => {
+    try {
+      const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.city.toLowerCase()}&appid=${apiKey}`)
+      const obj = await res.json()
+      setWeatherData(obj)
+    }catch(error){
+      throw error
+    }
+  }
+
   return (
-      <div className="results-page">
+    weatherData ? <Planet weatherData = {weatherData}/> :
+      (<div className="results-page">
             <img src={planet} className='planet'/>
         <div className="top">
           <h2> Brrrr. -3 c. Snow Shower?</h2>
@@ -30,6 +41,6 @@ export default function ResultsPage(props) {
           <h4>Freezing</h4>
       </div>
 
-      </div>
+      </div>)
   )
 }
