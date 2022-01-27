4c8be2e08562a3f62558ef5aa7b87044481bb7c8
Andrew Conte <andrew-conte434>
2022-01-27 14:54:23 -0500

    trying to implement routing

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,33 +1,29 @@
 import React, { useState } from 'react'
-import { Navigate } from 'react-router-dom'
 import logo from '../star-wars-logo.wine.svg'
 import video from '../videos/star-wars-video.mp4'
 import audio from '../audio/star-wars-music.mp3'
 import '../styles/App.css'
-import axios from 'axios'
-//import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { Context } from './Context'
+import { useNavigate } from 'react-router-dom'
 import ResultsPage from './ResultsPage'
-import Navbar from './Navbar'
 
 export default function HomePage() {
+
+	let navigate = useNavigate()
 
 	const [city, setCity] = useState('')
 	const [inputValue, setInputValue] = useState('')
 
+	const handleClick = () => {
+		setCity(inputValue)
+		navigate(`/${city}`)
+	}
+
 	return (
-		city ? <ResultsPage city={city} /> : (<div className='home-page'>
+		<div className='home-page'>
 
 			<video src={video} autoPlay muted id='video' />
 			<audio src={audio} autoPlay></audio>
-
-			{/* <audio src={audio} type='mp3' muted='false' autoPlay /> */}
-			{/* <audio src={audio} autoPlay></audio> */}
-
-			{/* <iframe id='video'
-				src="https://www.youtube.com/embed/MkRSmynqonM?start=50&autoplay=1&mute=1&modestbranding=1&fs=0&autohide=0&controls=0&showinfo=0"
-				title="YouTube video player" frameborder="0"
-				allowfullscreen>
-			</iframe> */}
 
 			<div className='logo-div'>
 				<img src={logo} className='star-wars-logo' alt='logo' />
@@ -35,11 +31,11 @@ export default function HomePage() {
 				<input placeholder='ENTER CITY HERE...'
 					onChange={(e) => setInputValue(e.target.value)}></input>
 				<button className='enter-button'
-					onClick={() => { setCity(inputValue) }}>
+					onClick={(e) => handleClick()}>
 					ENTER CITY
 				</button>
 			</div>
 
-		</div>)
+		</div>
 	)
 }
--- src/components/Planet.js
+++ src/components/Planet.js
@@ -1,32 +1,72 @@
 import React, { useEffect, useState } from 'react'
 import '../styles/Planet.css'
+import { Context } from './Context'
+import { useParams,useNavigate } from 'react-router-dom';
 import selectPlanet from '../modules/planetSelect';
 
 export default function (props) {
+    let params = useParams()
+    let navigate = useNavigate()
+
     const [planets, setPlanets] = useState(null)
     const [id, setId] = useState(1)
 
-    useEffect(async() => {
+    const [weatherData, setWeatherData] = useState(null)
+    const [error, setError] = useState(false)
+
+    const apiKey = '2633a1483698ea57695b55437e395ee8' //SHAKHRAM
+
+    useEffect(() => {
+        fetchWeather()
+    }, [])
+
+    const fetchWeather = async() => {
         try {
-            const res =  await fetch(`https://star-wars-weather-database.herokuapp.com/api/planets/`)
-            const obj =  await res.json()
+            console.log(params.city)
+            const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${params.city.toLowerCase()}&appid=${apiKey}`);
+            if (!res.ok) {
+                setError(true)
+            } else {
+                const obj = await res.json()
+                setError(false)
+                setWeatherData(obj)
+                console.log(obj)
+                console.log(weatherData)
+                fetchPlanets()
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const fetchPlanets = async () => {
+        try {
+            const res = await fetch(`https://star-wars-weather-database.herokuapp.com/api/planets/`)
+            const obj = await res.json()
             setPlanets(obj)
-            setId(selectPlanet(props.weatherData.main.temp, props.weather.clouds.all,
-                props.weatherData.weather[0].description)  - 1)
+            console.log(planets)
+            console.log(weatherData)
+            setId(selectPlanet(weatherData.main.temp, weatherData.clouds.all,
+                weatherData.weather[0].description)  - 1)
+            
         } catch (error) {
             throw error
         }
-    }, [])
+    }
     
     return (
-        planets && <div className='planet-div'>
+        (error === true) ? navigate('/error') : 
+        (planets) && 
+        <div className='planet-div'>
 
             <div>
                 <img className='planet-image' src={planets[id].imageUrl} alt='planet' />
             </div>
 
             <div className='top'>
-                <h1>Wow! It's 30 degrees? </h1>
+                <h1>{Math.floor(((weatherData.main.temp - 273.15) 
+                        * 1.8) + 32)} degrees?  
+                        {weatherData.weather[0].description}? </h1>
             </div>
 
             <div className='middle'>
@@ -36,7 +76,7 @@ export default function (props) {
             </div>
 
             <div className='bottom'>
-                <h1>Wind: 12mph, Sky: Overcast</h1>
+                <h1>{planets[id].quote}</h1>
             </div>
 
         </div>
